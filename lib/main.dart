729de9e9384cd3a37c7fd01d45257e561c911913
Hasan Yousaf <hasanyo@sheridancollege.ca>
2023-03-21 19:12:22 -0400

    added Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,75 +6,168 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Welcome Back!"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200, height: 150, child: Text('Logo goes here')),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.all(15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Email'),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Password'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage()));
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            Text('New User? Create Account') //Add navigation to sign up page
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Skinfo'),
-              backgroundColor: Colors.blue,
-            ),
-            body: const FirstScreen()));
+          appBar: AppBar(
+            title: const Text('Skinfo'),
+            backgroundColor: Colors.blue,
+          ),
+        ));
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
+// class FirstScreen extends StatelessWidget {
+//   const FirstScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Center(
+//             child: ElevatedButton(
+//       onPressed: () => Navigator.of(context)
+//           .push(MaterialPageRoute(builder: (context) => const NewScreen())),
+//       child: const Text('Second Screen'),
+//     )));
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-            child: ElevatedButton(
-      onPressed: () => Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => const NewScreen())),
-      child: const Text('Second Screen'),
-    )));
-  }
-}
-
-class NewScreen extends StatefulWidget {
-  const NewScreen({Key? key}) : super(key: key);
-
-  @override
-  State<NewScreen> createState() => _NewScreenState();
-}
-
-class _NewScreenState extends State<NewScreen> {
-  TextEditingController textEditingController = TextEditingController();
-
-  @override
-  void dispose() {
-    textEditingController.dispose();
-    // ignore: avoid_print
-    // print('Dispose used');
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Second Screen'),
-          backgroundColor: Colors.blue,
-        ),
-        // Avoid unnecessary containers
-        body: Container(
-            child: Center(
-                child: ElevatedButton(
-          onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => const FirstScreen())),
-          child: const Text('First Screen'),
-        ))));
-  }
-}
+// class NewScreen extends StatefulWidget {
+//   const NewScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<NewScreen> createState() => _NewScreenState();
+// }
+//
+// class _NewScreenState extends State<NewScreen> {
+//   TextEditingController textEditingController = TextEditingController();
+//
+//   @override
+//   void dispose() {
+//     textEditingController.dispose();
+//     // ignore: avoid_print
+//     // print('Dispose used');
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: Colors.white,
+//         appBar: AppBar(
+//           title: const Text('Second Screen'),
+//           backgroundColor: Colors.blue,
+//         ),
+//         // Avoid unnecessary containers
+//         body: Container(
+//             child: Center(
+//                 child: ElevatedButton(
+//           onPressed: () => Navigator.of(context).push(
+//               MaterialPageRoute(builder: (context) => const FirstScreen())),
+//           child: const Text('First Screen'),
+//         ))));
+//   }
+// }
